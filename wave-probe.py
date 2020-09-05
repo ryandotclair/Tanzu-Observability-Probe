92e090f739abddb076dad2ba6d8b169814029c3c
Ryan Clair <ryandotclair@users.noreply.github.com>
2020-09-05 10:41:14 -0500

    Update wave-probe.py
    Removed wavefront references

--- wave-probe.py
+++ wave-probe.py
@@ -6,27 +6,27 @@ import logging
 
 # DEPENDENCIES:
 # This assumes you have the following environment variables set:
-#   - WFPAUTH (for your Wavefront API token)
-#   - WFPEVENTID (the unique alert ID that you're probing for... this is found in wavefront UI by selecting the 
+#   - TO_TOKEN (for your Wavefront API token)
+#   - TO_EVENTID (the unique alert ID that you're probing for... this is found in wavefront UI by selecting the 
 # alert in question and grabbing the number after "../alerts/#")
-#   - WFPBASEURL (ex: surf.wavefront.com ... do not include "/")
+#   - TO_BASEURL (ex: surf.wavefront.com ... do not include "/")
 #   - SCRIPT_RESPONSE_FILE (this is created by CodeStream)
 #
 # This also assumes the script is ran in a CodeStream ssh task, which requires you to run the following commands 
 # _before_ you execute this python script:
 #
-# export WFPAUTH=[API TOKEN]
-# export WFPEVENTID=[ALERT'S ID]
-# export WFPBASEURL=$ [x.wavefront.com]
+# export TO_TOKEN=[API TOKEN]
+# export TO_EVENTID=[ALERT'S ID]
+# export TO_BASEURL=$ [x.wavefront.com]
 # export RESPONSE=$SCRIPT_RESPONSE_FILE
 #
 # The preceeding CodeStream tasks can then check the response file for FIRING (alert was triggered). 
 # Otherwise after it will say "NORMAL" after 4 minutes (tunable by changing loop count and/or sleep count)
 
 #ENV VARs
-wvAuth = "Bearer " + str(os.environ['WFPAUTH']) #your Wavefront API token
-eventID = os.environ['WFPEVENTID'] # unique alert ID that we're probing for
-baseURL = os.environ['WFPBASEURL'] # example: surf.wavefront.com
+TOAuth = "Bearer " + str(os.environ['TO_TOKEN']) #your Wavefront API token
+eventID = os.environ['TO_EVENTID'] # unique alert ID that we're probing for
+baseURL = os.environ['TO_BASEURL'] # example: surf.wavefront.com
 
 #CONFIG
 counter = 1
@@ -35,7 +35,7 @@ normal_cmd = 'echo NORMAL > $RESPONSE'
 url = "https://" + baseURL + "/api/v2/alert/" + str(eventID)
 head = {
     'Accept':'application/json',
-    'Authorization': wvAuth
+    'Authorization': TOAuth
 }
 
 # Check first 5min of deployment if there's an issue
